55ce4769c0bf5db415434d412d13bafa46693d54
andrew <arandel@uwo.ca>
2018-03-14 12:17:00 -0400

    Test Drive Code Added

---
+++ Project/Code/DriveCode/DriveCode.ino
@@ -0,0 +1,21 @@
+#include <Servo.h>
+Servo leftMotor;
+Servo rightMotor;
+
+#define LEFT_MOTOR PA9
+#define RIGHT_MOTOR PA7
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LEFT_MOTOR, OUTPUT);
+  pinMode(RIGHT_MOTOR, OUTPUT);
+  leftMotor.attach(LEFT_MOTOR);
+  rightMotor.attach(RIGHT_MOTOR);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  leftMotor.writeMicroseconds(1800);
+  rightMotor.writeMicroseconds(1800);
+}
