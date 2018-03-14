56cacb4ff3e5cbe0d5e60ad44d6a522b79ed678f
Andrew <arandel@uwo.ca>
2018-03-14 14:08:44 -0400

    Fan's Code

---
+++ Project/ProjectCode/ProjectCode.ino
@@ -0,0 +1,43 @@
+#include <Servo.h>
+
+Servo leftDrive;
+Servo rightDrive;
+Servo pyramidLeft;
+Servo pyramidRight;
+Servo pyramidLift;
+int frontUltPing = PC0, frontUltData = PC1, rightUltPing = PB0, rightUltData = PA4;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  leftDrive.attach(PA9);
+  rightDrive.attach(PA7);
+  pyramidLeft.attach();
+  pyramidRight.attach();
+  pyramidLift.attach();
+
+  pinMode(frontUltPing, OUTPUT);
+  pinMode(frontUltData, INPUT);
+  pinMode(rightUltPing, OUTPUT);
+  pinMode(rightUltData, INPUT);
+  
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void turnLeft(int deg) {
+  
+  
+}
+
+void turnRight(int deg) {
+  
+}
+
+
+
