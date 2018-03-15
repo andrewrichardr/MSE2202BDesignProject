046dda7884a70b109e4ef168b6f846a632b998be
andrew <arandel@uwo.ca>
2018-03-15 12:02:36 -0400

    Test Drive Code modified

--- Project/Code/DriveCode/DriveCode.ino
+++ Project/Code/DriveCode/DriveCode.ino
@@ -2,8 +2,8 @@
 Servo leftMotor;
 Servo rightMotor;
 
-#define LEFT_MOTOR PA9
-#define RIGHT_MOTOR PA7
+#define LEFT_MOTOR 9
+#define RIGHT_MOTOR 8
 
 void setup() {
   // put your setup code here, to run once:
@@ -18,4 +18,11 @@ void loop() {
   // put your main code here, to run repeatedly:
   leftMotor.writeMicroseconds(1800);
   rightMotor.writeMicroseconds(1800);
+delay(500);
+    leftMotor.writeMicroseconds(1700);
+  rightMotor.writeMicroseconds(1300);
+delay(2000);
+    leftMotor.writeMicroseconds(1800);
+  rightMotor.writeMicroseconds(1800);
+  delay(2000);
 }
