31448a25050239abf727600d95a89e6ca22c7b36
fkang3 <37484115+fkang3@users.noreply.github.com>
2018-03-17 15:25:08 -0400

    Code to read IR input

---
+++ Project/ProjectCode/IR_MSEBoard.ino
@@ -0,0 +1,20 @@
+#include <IRremote.h>
+#include <IRremoteInt.h>
+
+#define RECV_PIN 7
+IRrecv receiver(RECV_PIN);
+decode_results results;
+unsigned char ledTx; 
+
+void setup() {
+  // put your setup code here, to run once:
+  receiver.enableIRIn();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (receiver.decode(&results)) { 
+    Serial.write(ledTx);
+    receiver.resume();
+  }
+}
