1f84d185d2eb71ebf6a05188a1c6d63f2bcfb952
andrew <arandel@uwo.ca>
2018-03-22 14:17:35 -0400

    DriveCode update

--- Project/_Prototype_Code/CompassTest/CompassTest.ino
+++ Project/_Prototype_Code/CompassTest/CompassTest.ino
@@ -1,51 +1,26 @@
-#include <Wire.h> //I2C Arduino Library
+#include <SparkFun_MAG3110.h>
 
-#define address 0x1E //0011110b, I2C 7bit address of HMC5883
+MAG3110 mag = MAG3110(); //Instantiate MAG3110
 
-void setup(){
-  //Initialize Serial and I2C communications
+void setup() {
   Serial.begin(9600);
-  Wire.begin();
-  
-  //Put the HMC5883 IC into the correct operating mode
-  Wire.beginTransmission(address); //open communication with HMC5883
-  Wire.write(0x02); //select mode register
-  Wire.write(0x00); //continuous measurement mode
-  Wire.endTransmission();
-  pinMode(2, OUTPUT);
-  digitalWrite(2, HIGH);
-  pinMode(13, OUTPUT);
-  digitalWrite(13, HIGH);
-  pinMode(PA9, OUTPUT);
-  digitalWrite(PA9, HIGH);
+
+  mag.initialize(); //Initializes the mag sensor
+  mag.start();      //Puts the sensor in active mode
 }
 
- void loop(){
-  
-  int x,y,z; //triple axis data
+void loop() {
 
-  //Tell the HMC5883L where to begin reading data
-  Wire.beginTransmission(address);
-  Wire.write(0x03); //select register 3, X MSB register
-  Wire.endTransmission();
-  
- 
- //Read data from each axis, 2 registers per axis
-  Wire.requestFrom(address, 6);
-  if(6<=Wire.available()){
-    x = Wire.read()<<8; //X msb
-    x |= Wire.read(); //X lsb
-    z = Wire.read()<<8; //Z msb
-    z |= Wire.read(); //Z lsb
-    y = Wire.read()<<8; //Y msb
-    y |= Wire.read(); //Y lsb
+  int x, y, z;
+  //Only read data when it's ready
+  if(mag.dataReady()) {
+    //Read the data
+    mag.readMag(&x, &y, &z);
+
+    Serial.print(x);
+    Serial.print(", ");
+    Serial.print(y);
+    Serial.print(", ");
+    Serial.println(z);
   }
-  
-  //Print out values of each axis
-  Serial.print("x: ");
-  Serial.print(x);
-  Serial.print("  y: ");
-  Serial.print(y);
-  Serial.print("  z: ");
-  Serial.println(z);
 }
--- Project/_Prototype_Code/DriveCode/DriveCode.ino
+++ Project/_Prototype_Code/DriveCode/DriveCode.ino
@@ -9,10 +9,12 @@ Servo rightMotor;
 #define LEFT_MOTOR PA9
 #define RIGHT_MOTOR PC7
 
-#define FR_ULTRASONIC_IN PC1
-#define FR_ULTRASONIC_OUT PC0
+#define LR_ULTRASONIC_IN PC1
+#define LR_ULTRASONIC_OUT PC0
 #define LF_ULTRASONIC_IN PA4
 #define LF_ULTRASONIC_OUT PB0
+#define F_ULTRASONIC_IN PA0
+#define F_ULTRASONIC_OUT PA1
 
 #define WALL_LOWER_THRESHOLD 5
 #define WALL_UPPER_THRESHOLD 10
@@ -26,38 +28,17 @@ Servo rightMotor;
 
 #define COMPASS_I2C_ADDR 0x1E
 
-long FR_ultrasonic_dist;
+long LR_ultrasonic_dist;
 long LF_ultrasonic_dist;
-unsigned long FRecho;
+long F_ultrasonic_dist;
+unsigned long LRecho;
 unsigned long LFecho;
+unsigned long Fecho;
 
 int x, y, z;
 
 void lmswitch(){
   
-  Wire.beginTransmission(COMPASS_I2C_ADDR);
-  Wire.write(0x03); //select register 3, X MSB register
-  Wire.endTransmission();
-  
- 
- //Read data from each axis, 2 registers per axis
-  Wire.requestFrom(COMPASS_I2C_ADDR, 6);
-  if(6<=Wire.available()){
-    x = Wire.read()<<8; //X msb
-    x |= Wire.read(); //X lsb
-    z = Wire.read()<<8; //Z msb
-    z |= Wire.read(); //Z lsb
-    y = Wire.read()<<8; //Y msb
-    y |= Wire.read(); //Y lsb
-  }
-  
-  //Print out values of each axis
-  Serial.print("x: ");
-  Serial.print(x);
-  Serial.print("  y: ");
-  Serial.print(y);
-  Serial.print("  z: ");
-  Serial.println(z);
   
 }
 
@@ -65,36 +46,40 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
 
-  pinMode(FR_ULTRASONIC_IN, OUTPUT);
+  pinMode(LR_ULTRASONIC_IN, OUTPUT);
   pinMode(LF_ULTRASONIC_IN, OUTPUT);
-  pinMode(FR_ULTRASONIC_OUT, INPUT);
+  pinMode(F_ULTRASONIC_IN, OUTPUT);
+  pinMode(LR_ULTRASONIC_OUT, INPUT);
   pinMode(LF_ULTRASONIC_OUT, INPUT);
+  pinMode(F_ULTRASONIC_OUT, INPUT);
+
 
   pinMode(LEFT_MOTOR, OUTPUT);
   pinMode(RIGHT_MOTOR, OUTPUT);
   leftMotor.attach(LEFT_MOTOR);
   rightMotor.attach(RIGHT_MOTOR);
 
-  attachInterrupt(digitalPinToInterrupt(LIMIT_SW), lmswitch, FALLING);
+ // attachInterrupt(digitalPinToInterrupt(LIMIT_SW), lmswitch, FALLING);
 
-  Wire.begin();
-  Wire.beginTransmission(COMPASS_I2C_ADDR);
-  Wire.write(0x02);
-  Wire.write(0x00);
-  Wire.endTransmission();
+  //Wire.begin();
+  //Wire.beginTransmission(COMPASS_I2C_ADDR);
+  //Wire.write(0x02);
+ // Wire.write(0x00);
+//  Wire.endTransmission();
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  pingFR();
+  pingLR();
   pingLF();
+  pingF();
 
-  if(abs(LF_ultrasonic_dist - FR_ultrasonic_dist) <= 15){
+  if(abs(LF_ultrasonic_dist - LR_ultrasonic_dist) <= 50){
     goForward();
     Serial.print("Forward  ");
   }
-  else if(LF_ultrasonic_dist < FR_ultrasonic_dist){
+  else if(LF_ultrasonic_dist > LR_ultrasonic_dist){
     moveIn();
     Serial.print("Move In  ");
   }
@@ -103,24 +88,30 @@ void loop() {
     Serial.print("Move Out  ");
   }
 
-  Serial.print("LF: ");
+  if(F_ultrasonic_dist < 1000){
+    TurnOnAxis();
+  }
+  
+  Serial.print("F: ");
+  Serial.print(F_ultrasonic_dist);
+  Serial.print(" LF: ");
   Serial.print(LF_ultrasonic_dist);
-  Serial.print("  FR: ");
-  Serial.println(FR_ultrasonic_dist);
+  Serial.print("  LR: ");
+  Serial.println(LR_ultrasonic_dist);
 
 }
 
-void pingFR(){
+void pingLR(){
 
   //Ping Ultrasonic
   //Send the Ultrasonic Range Finder a 10 microsecond pulse per tech spec
-  digitalWrite(FR_ULTRASONIC_IN, HIGH);
+  digitalWrite(LR_ULTRASONIC_IN, HIGH);
   delayMicroseconds(10);  //The 10 microsecond pause where the pulse in "high"
-  digitalWrite(FR_ULTRASONIC_IN, LOW);
+  digitalWrite(LR_ULTRASONIC_IN, LOW);
   //use command pulseIn to listen to Ultrasonic_Data pin to record the
   //time that it takes from when the Pin goes HIGH until it goes LOW 
-  FRecho = pulseIn(FR_ULTRASONIC_OUT, HIGH, 10000);
-  if(FRecho) FR_ultrasonic_dist = FRecho;
+  LRecho = pulseIn(LR_ULTRASONIC_OUT, HIGH, 10000);
+  if(LRecho) LR_ultrasonic_dist = LRecho;
 }
 
 void pingLF(){
@@ -136,10 +127,23 @@ void pingLF(){
   if(LFecho) LF_ultrasonic_dist = LFecho;
 }
 
-void TurnOnAxis(int deg){
+void pingF(){
+
+  //Ping Ultrasonic
+  //Send the Ultrasonic Range Finder a 10 microsecond pulse per tech spec
+  digitalWrite(F_ULTRASONIC_IN, HIGH);
+  delayMicroseconds(10);  //The 10 microsecond pause where the pulse in "high"
+  digitalWrite(F_ULTRASONIC_IN, LOW);
+  //use command pulseIn to listen to Ultrasonic_Data pin to record the
+  //time that it takes from when the Pin goes HIGH until it goes LOW 
+  Fecho = pulseIn(F_ULTRASONIC_OUT, HIGH, 10000);
+  if(Fecho) F_ultrasonic_dist = Fecho;
+}
+
+void TurnOnAxis(){
   leftMotor.writeMicroseconds(1250);
   rightMotor.writeMicroseconds(1750);
-  delay(500); 
+  delay(1000); 
 }
 
 void goForward(){
